9cb6d89fdd276fff07d67eede75c29551f1c4b78
CoffeeIsCode <112358010+CoffeeIsCode@users.noreply.github.com>
2023-03-18 02:32:24 -0400

    Create phase_update_1.js

---
+++ phase_update_1.js
@@ -0,0 +1,24 @@
+//   LOGICAL READER PHRASE ADD-ON
+//
+//   Coded by: CoffeeIsCode
+//   Copyright (C) 2023 - Caffeinated Software Systems - All Rights Reserved
+//
+
+
+
+
+
+
+var id = 0;
+var phrase = {};
+phrase.id = [];
+phrase.searched = [];
+phrase.found = [];
+phrase.alt = []
+
+Mozilla Public License
+
+// Global Application Data
+
+
+// System Functions
